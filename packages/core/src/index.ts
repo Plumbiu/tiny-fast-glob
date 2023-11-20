26dea7dc30cfe8b580d94e4db7c6641a7f0845df
Plumbiu <3434909403@qq.com>
2023-11-20 17:57:06 +0800

    fix: read undefined

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -19,7 +19,10 @@ export function glob(pattern: string, options: Options = {}) {
   )
 
   while (stack.length > 0) {
-    const p = stack.shift()!
+    const p = stack.shift()
+    if (!p) {
+      return
+    }
     if (p.path.includes('.')) {
       continue
     }
@@ -42,3 +45,11 @@ export function glob(pattern: string, options: Options = {}) {
 
   return absolute ? result : result.map((item) => path.relative(cwd, item))
 }
+
+console.log(
+  glob('**/.env*', {
+    cwd: 'D:\\Code\\Project\\@plumbiu\\blog',
+    absolute: true,
+    ignore: ['**/node_modules/**', '**/dist/**'],
+  }),
+)
