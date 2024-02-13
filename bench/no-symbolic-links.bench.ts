d63249e594bba3becc6fda070cc5a93754da20d8
Plumbiu <3434909403@qq.com>
2024-02-13 19:32:27 +0800

    test: add more bench file

---
+++ bench/no-symbolic-links.bench.ts
@@ -0,0 +1,36 @@
+import { bench } from 'vitest'
+import { glob as fast_glob } from 'fast-glob'
+import tiny_glob from 'tiny-glob'
+import { glob as tiny_fast_glob } from '../src/index'
+
+bench(
+  'fast-glob',
+  async () => {
+    await fast_glob('**/*.js', {
+      absolute: true,
+      followSymbolicLinks: false,
+    })
+  },
+  { time: 250 },
+)
+
+bench(
+  'tiny-glob',
+  async () => {
+    await tiny_glob('**/*.js', {
+      absolute: true,
+      filesOnly: true,
+    })
+  },
+  { time: 250 },
+)
+
+bench(
+  'tiny-fast-glob',
+  async () => {
+    await tiny_fast_glob('**/*.js', {
+      absolute: true,
+    })
+  },
+  { time: 250 },
+)
