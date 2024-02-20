8420cf07b793a179ec23a9dd3cebb153a9ab0332
Plumbiu <3434909403@qq.com>
2024-02-20 09:15:17 +0800

    test: simplify code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,161 +1,82 @@
-import { expect, test } from 'vitest'
-import fast_glob from 'fast-glob'
-import { glob as tiny_fast_glob } from '../src/index'
-import { slashMap } from './utils'
+import { expect, test, describe } from 'vitest'
+import fg from 'fast-glob'
+import { Options, glob as tfg } from '../src/index'
+import { diffSet } from './utils'
 
-test('absolute', async () => {
-  const r1 = await fast_glob('**/*.js', {
-    followSymbolicLinks: false,
-    absolute: true,
+export async function fn(p: string | string[], opts: Options = {}) {
+  const r1 = await fg(p, opts)
+  const r2 = await tfg(p, opts)
+  expect(diffSet(r1, r2)).toEqual([])
+}
+
+describe('options', () => {
+  test('absolute', async () => {
+    await fn('**/*.js', {
+      absolute: true,
+    })
   })
 
-  const r2 = await tiny_fast_glob('**/*.js', {
-    absolute: true,
+  test('cwd', async () => {
+    await fn('**/*.ts', {
+      cwd: 'src',
+    })
   })
 
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
+  test('followSymbolicLinks', async () => {
+    await fn('**/*.js', {
+      followSymbolicLinks: false,
+    })
+  })
+
+  test('filesOnly', async () => {
+    await fn(['**/*.js', '**/@**'], {
+      onlyFiles: false,
+    })
+  })
+
+  test('dot', async () => {
+    await fn('node_modules/**/*.js', {
+      dot: true,
+    })
+  })
 })
 
-test('cwd', async () => {
-  const r1 = await fast_glob('**/*.js', {
-    cwd: 'src',
-    followSymbolicLinks: false,
-    absolute: true,
+describe('path', () => {
+  test('**', async () => {
+    await fn('**')
   })
 
-  const r2 = await tiny_fast_glob('**/*.js', {
-    cwd: 'src',
-    absolute: true,
+  test('**/*', async () => {
+    await fn('**/*')
   })
 
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
+  test('./', async () => {
+    await fn(['./tsup/**/*.js', './eslint/**/*.js'])
+  })
+
+  test('**/* with ignore', async () => {
+    await fn(['./tsup/**/*.js', './eslint/**/*.js'], {
+      ignore: ['*eslint*', '@types/**/*', 'dist/**'],
+    })
+  })
+
+  test('./ with cwd and absolute', async () => {
+    await fn('./fast-glob/*', {
+      cwd: 'D:\\Code\\Project\\@plumbiu\\tiny-fast-glob\\node_modules',
+    })
+    await fn('./fast-glob/*', {
+      cwd: 'D:\\Code\\Project\\@plumbiu\\tiny-fast-glob\\node_modules',
+      absolute: true,
+    })
+  })
+
+  test('bracket', async () => {
+    await fn('test/brackets (foo)/**/*.ts')
+  })
 })
 
-test('followSymbolicLinks', async () => {
-  const r1 = await fast_glob('**/*.js', {
-    absolute: true,
+describe('may bug', () => {
+  test('unicode', async () => {
+    await fn('test/**/*.ts')
   })
-
-  const r2 = await tiny_fast_glob('**/*.js', {
-    absolute: true,
-    followSymbolicLinks: true,
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('filesOnly', async () => {
-  const r1 = await fast_glob(['**/*.js', '**/@**'], {
-    absolute: true,
-    onlyFiles: false,
-  })
-
-  const r2 = await tiny_fast_glob(['**/*.js', '**/@**'], {
-    absolute: true,
-    followSymbolicLinks: true,
-    onlyFiles: false,
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('dot', async () => {
-  const r1 = await fast_glob('**/*.ts', {
-    absolute: true,
-    onlyFiles: false,
-    dot: true,
-  })
-
-  const r2 = await tiny_fast_glob('**/*.ts', {
-    absolute: true,
-    followSymbolicLinks: true,
-    onlyFiles: false,
-    dot: true,
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('**', async () => {
-  const r1 = await fast_glob('**', {
-    cwd: 'node_modules',
-    followSymbolicLinks: false,
-    onlyFiles: true,
-  })
-
-  const r2 = await tiny_fast_glob('**', {
-    cwd: 'node_modules',
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('**/*', async () => {
-  const r1 = await fast_glob('**/*', {
-    cwd: 'node_modules',
-    followSymbolicLinks: false,
-    onlyFiles: true,
-  })
-
-  const r2 = await tiny_fast_glob('**/*', {
-    cwd: 'node_modules',
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('./', async () => {
-  const r1 = await fast_glob(['./tsup/**/*.js', './eslint/**/*.js'], {
-    cwd: 'node_modules',
-    onlyFiles: true,
-  })
-
-  const r2 = await tiny_fast_glob(['./tsup/**/*.js', './eslint/**/*.js'], {
-    cwd: 'node_modules',
-    followSymbolicLinks: true,
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('unicode', async () => {
-  const r1 = await fast_glob('test/**/*.ts', {
-    onlyFiles: true,
-  })
-
-  const r2 = await tiny_fast_glob('test/**/*.ts', {
-    followSymbolicLinks: true,
-  })
-
-  expect(slashMap(r2).sort()).toEqual(r1.sort())
-})
-
-test('**/* by ignore', async () => {
-  const r1 = await fast_glob(['**/*.js', '**/*.ts'], {
-    ignore: ['*eslint*', '@types/**/*', 'dist/**'],
-    followSymbolicLinks: false,
-    onlyFiles: true,
-  })
-
-  const r2 = await tiny_fast_glob(['**/*.js', '**/*.ts'], {
-    ignore: ['*eslint*', '@types/**/*', 'dist/**'],
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
-})
-
-test('cwd with ./', async () => {
-  const r1 = await fast_glob('./fast-glob/*', {
-    cwd: 'D:\\Code\\Project\\@plumbiu\\tiny-fast-glob\\node_modules',
-    followSymbolicLinks: false,
-    onlyFiles: true,
-    absolute: true,
-  })
-
-  const r2 = await tiny_fast_glob('./fast-glob/*', {
-    absolute: true,
-    cwd: 'D:\\Code\\Project\\@plumbiu\\tiny-fast-glob\\node_modules',
-  })
-
-  expect(slashMap(r2.sort())).toEqual(r1.sort())
 })
--- test/utils.ts
+++ test/utils.ts
@@ -6,3 +6,9 @@ export function slash(p: string) {
 export function slashMap(ps: string[]) {
   return ps.map((item) => slash(item))
 }
+
+export function diffSet(fastglbResult: string[], tinyfastglobResult: string[]) {
+  const a = new Set(fastglbResult)
+  const b = new Set(slashMap(tinyfastglobResult))
+  return [...new Set([...a].filter((x) => !b.has(x)))]
+}
